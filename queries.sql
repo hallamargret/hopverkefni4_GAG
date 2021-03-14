d4665df0cfa8c1fe44f0348290851d24586683ea
hallamargret <hallamargret99@gmail.com>
2021-03-14 16:14:12 +0000

    2 komid

--- queries.sql
+++ queries.sql
@@ -1,6 +1,93 @@
-CREATE DATABASE PIV;
+--CREATE DATABASE PIV;
 
-SELECT A.AgentID, A.codename, A.status, COUNT(C.CaseID)
+
+-- 1.
+select
+    1 as Query;
+
+CREATE OR REPLACE FUNCTION mostCommonLocationAgentCaseLed()
+
+
+
+
+
+CREATE OR REPLACE VIEW AS infoAgent AS
+SELECT A.codename, A.status, COUNT(C.CaseID)
 FROM Agents A
 NATURAL JOIN Cases C
-GROUP BY A.AgentID;
+GROUP BY A.AgentID
+HAVING ()
+
+
+SELECT * FROM infoAgent;
+
+
+
+select
+    2 as Query;
+
+SELECT P.PersonID, P.name, L.location
+FROM People P
+INNER JOIN Locations L ON P.LocationID = L.LocationID
+INNER JOIN InvolvedIn I ON P.PersonID = I.PersonID
+GROUP BY P.PersonID, L.LocationID
+HAVING L.location = 'Stokkseyri'
+ORDER BY COUNT(distinct I.CaseID) DESC
+LIMIT 3;
+
+
+
+
+
+-- 3.
+
+
+-- 4.
+
+
+-- 5.
+CREATE OR REPLACE FUNCTION CaseCountFixer
+RETURNS TRIGGER --CaseCountTracker
+LANGUAGE plpgsql
+AS $$
+DECLARE r Locations%rowtype;
+BEGIN 
+    FOR r IN
+        SELECT L.LocationID, L.caseCount 
+        FROM Locations L
+    LOOP
+        r.caseCount = (SELECT COUNT(C.CaseID)
+                        FROM Cases C
+                        WHERE C.LocationID = r.LocationID) 
+
+    RETURN NEW;
+    --ATH
+    --TODO: correct the caseCount
+$$;
+
+
+
+-- 6. Create a trigger CaseCountTracker that whenever a new case is created, deleted or its
+-- location attribute is updated, runs CaseCountFixer
+DROP TRIGGER IF EXISTS CaseCountTracker ON Cases;
+CREATE TRIGGER CaseCountTracker
+    AFTER INSERT OR DELETE ON Cases --OR AFTER UPDATE ON Cases.LocationID 
+    FOR EACH ROW
+    EXECUTE FUNCTION CaseCountFixer();
+
+
+
+
+
+
+BEGIN;
+INSERT INTO Cases
+VALUES(DEFAULT,'test case', false, 2021,1,2);
+
+
+ROLLBACK;
+
+--7.
+
+
+--8.

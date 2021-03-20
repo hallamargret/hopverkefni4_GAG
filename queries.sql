cec89b33dff8e8f3cdf0448df86299e97b754426
evasolp <evap19@ru.is>
2021-03-20 17:16:15 +0000

    Allt komið

--- queries.sql
+++ queries.sql
@@ -1,77 +1,76 @@
 --CREATE DATABASE PIV;
 
+-- Eva Sól Pétursdóttir og Halla Margrét Jónsdóttir
 
 -- 1.
 select
     1 as Query;
 
 CREATE OR REPLACE VIEW infoAgent AS
-SELECT A.codename, A.status, COUNT(C.CaseID) AS "Number of cases", ARRAY(SELECT L1.location 
-                                                    FROM Locations L1
-                                                    INNER JOIN Cases C1 ON C1.locationID = L1.locationID
-                                                    GROUP BY C1.AgentID, L1.LocationID HAVING C1.AgentID = A.AgentID AND COUNT(C1.LocationID) >= ALL (
-                                                    SELECT COUNT(C2.locationID)
-                                                    FROM Cases C2
-                                                    GROUP BY C2.AgentID, C2.locationID HAVING C2.AgentID = A.AgentID
+SELECT 
+    A.codename, 
+    A.status, 
+    COUNT(C.CaseID) AS "Number of cases", 
+    ARRAY(SELECT L1.location 
+        FROM Locations L1
+        INNER JOIN Cases C1 ON C1.locationID = L1.locationID
+        GROUP BY C1.AgentID, L1.LocationID HAVING C1.AgentID = A.AgentID AND COUNT(C1.LocationID) >= ALL (
+                    SELECT COUNT(C2.locationID)
+                    FROM Cases C2
+                    GROUP BY C2.AgentID, C2.locationID HAVING C2.AgentID = A.AgentID
 )) AS "Most common locations led cases"
 FROM Agents A
 NATURAL JOIN Cases C
 GROUP BY A.AgentID;
 
-SELECT * FROM infoAgent;
+--SELECT * FROM infoAgent;
 
 
 select
     2 as Query;
 
 CREATE OR REPLACE VIEW top3subjectsStokkseyri AS
-SELECT P.PersonID, P.name, L.location
+SELECT 
+    P.PersonID, 
+    P.name, 
+    L.location
 FROM People P
 INNER JOIN Locations L ON P.LocationID = L.LocationID
 INNER JOIN InvolvedIn I ON P.PersonID = I.PersonID
-GROUP BY P.PersonID, L.LocationID
+GROUP BY 
+    P.PersonID, 
+    L.LocationID
 HAVING L.location = 'Stokkseyri'
 ORDER BY COUNT(distinct I.CaseID) DESC
 LIMIT 3;
 
-SELECT * FROM top3subjectsStokkseyri;
+--SELECT * FROM top3subjectsStokkseyri;
 
--- 3.
+
+select
+    3 as Query;
+
 CREATE OR REPLACE VIEW allNemeses AS
-SELECT DISTINCT ON (P.PersonId) A.AgentID, A.codename, P.PersonID, P.name
+SELECT 
+    DISTINCT ON (P.PersonId) A.AgentID, 
+    A.codename, 
+    P.PersonID, 
+    P.name
 FROM People P
 INNER JOIN InvolvedIn I ON I.PersonID = P.PersonID
 INNER JOIN Agents A ON I.AgentID = A.AgentID
-GROUP BY P.PersonID, A.AgentID, I.isCulprit
+GROUP BY 
+    P.PersonID, 
+    A.AgentID, 
+    I.isCulprit
 HAVING I.isCulprit = TRUE AND COUNT(I.isCulprit = TRUE) > 1
 ORDER BY P.PersonID, COUNT(A.AgentID) DESC;
 
---DROP VIEW IF EXISTS allNemeses;
 
+--SELECT * FROM allNemeses;
 
-SELECT * FROM allNemeses;
-
-BEGIN;
-INSERT INTO InvolvedIn
-VALUES(374, 1, 44, true);
-INSERT INTO InvolvedIn
-VALUES(374, 2, 44, true);
-
-SELECT * FROM allNemeses;
-
-INSERT INTO InvolvedIn
-VALUES(374, 3, 22, true);
-SELECT * FROM allNemeses;
-INSERT INTO InvolvedIn
-VALUES(374, 4, 44, true);
-SELECT * FROM allNemeses;
-
-ROLLBACK;
-
-
-
-
--- 4.
+select
+    4 as Query;
 
 CREATE OR REPLACE PROCEDURE InsertPerson(
     Pname_in varchar(255),
@@ -111,26 +110,11 @@ $$
 LANGUAGE plpgsql;
 
 
-BEGIN;
-CALL InsertPerson('thoka', 'voffi', 'Female', 'Akranes');
+select
+    5 as Query;
 
-SELECT * FROM PEOPLE
-WHERE name = 'thoka';
-SELECT * FROM Professions;
---ROLLBACK;
+--DROP PROCEDURE IF EXISTS CaseCountFixer();
 
-CALL InsertPerson('  ', 'voffi', 'Female', 'Akranes');
-SELECT * FROM PEOPLE
-WHERE name = '  ';
-SELECT * FROM Genders;
-
-ROLLBACK;
-
-
-
-
--- 5.
-DROP PROCEDURE IF EXISTS CaseCountFixer();
 CREATE OR REPLACE PROCEDURE CaseCountFixer()
 LANGUAGE SQL
 AS $$
@@ -141,16 +125,10 @@ AS $$
         WHERE C.LocationID = L.LocationID);
 $$;
 
-BEGIN;
-CALL CaseCountFixer();
-SELECT * FROM Locations;
-
-ROLLBACK;
 
 
-
--- 6. Create a trigger CaseCountTracker that whenever a new case is created, deleted or its
--- location attribute is updated, runs CaseCountFixer
+select
+    6 as Query;
 
 CREATE OR REPLACE FUNCTION TriggerCallsCaseCountFixer()
 RETURNS TRIGGER
@@ -162,47 +140,18 @@ BEGIN
 END;
 $$;
 
+--DROP TRIGGER IF EXISTS CaseCountTracker ON Cases;
 
-
-
-DROP TRIGGER IF EXISTS CaseCountTracker ON Cases;
 CREATE TRIGGER CaseCountTracker
-    AFTER INSERT OR DELETE OR UPDATE OF locationID ON Cases  --OR AFTER UPDATE ON Cases.LocationID 
+    AFTER INSERT OR DELETE OR UPDATE OF locationID ON Cases
     FOR EACH ROW
     EXECUTE FUNCTION TriggerCallsCaseCountFixer();
 
 
 
-SELECT * FROM Cases WHERE CaseID = 1;
+select
+    7 as Query;
 
-
-BEGIN;
-
-SELECT * FROM Locations;
-INSERT INTO Cases(CaseID, title, isClosed, year, AgentID, LocationID)
-VALUES(DEFAULT,'test case', false, 2021,1,91);
---ROLLBACK;
-
-SELECT * FROM Locations;
-SELECT * FROM Cases;
-
-UPDATE Cases 
-SET isClosed = false
-WHERE CaseID = 1;
-SELECT * FROM Cases;
---SELECT * FROM Locations;
-
---DELETE FROM InvolvedIn
---WHERE CaseID = 1;
-
---DELETE FROM Cases
---WHERE CaseID = 1;
-
---SELECT * FROM Locations;
-
-ROLLBACK;
-
---7.
 CREATE OR REPLACE function startInvestigation(
     agentID_in INT,
     personID_in INT,
@@ -241,10 +190,8 @@ LANGUAGE plpgsql;
 
 
 
-
-
-
---8.
+select
+    8 as Query;
 
 CREATE OR REPLACE FUNCTION defectiveAgentArrangements()
 RETURNS TRIGGER
@@ -254,8 +201,6 @@ DECLARE currCase INT;
 BEGIN
     -- the cases the old agent led goes to the agent with the fewest cases to lead
     
-    -- ath for loop go through cases and set it to agent with fewest cases each time
-    -- FOR 
     FOR currCase in (SELECT C.CaseID 
                     FROM Cases C 
                     WHERE C.AgentID = OLD.AgentID) loop
@@ -267,7 +212,7 @@ BEGIN
                         HAVING A.AgentID != OLD.AgentID
                         ORDER BY COUNT(A.AgentID), A.designation ASC 
                         LIMIT 1)
-        WHERE C.AgentID = currCase;
+        WHERE C.CaseID = currCase;
     END LOOP;
 
     -- The people who the agent was investigating will not be investigated by anyone anymore
@@ -285,30 +230,6 @@ BEGIN
 END;
 $$;
 
-
-
-
-CREATE OR REPLACE FUNCTION afterDeleteArrangements()
-RETURNS TRIGGER
-LANGUAGE plpgsql
-AS $$
-BEGIN
-
-
--- Delete any secret identity of the agent 
-    DELETE FROM People P 
-    WHERE P.PersonID = OLD.secretIdentity;
-
-
-    -- Adding the agent back to the database with status ghost and reversed codename after it is deleted
-    INSERT INTO Agents(AgentID, codename, designation, killLicense, status, secretIdentity, GenderID)
-    VALUES(default, recurReverseCodename(OLD.codename), OLD.designation, OLD.killLicense, 'ghost', NULL, OLD.GenderID);
-
-    RETURN OLD;
-END;
-$$;
-
-
 CREATE OR REPLACE FUNCTION recurReverseCodename(oldCodename varchar(255))
 RETURNS varchar(255)
 LANGUAGE plpgsql
@@ -321,55 +242,41 @@ BEGIN
 END;
 $$;
 
+CREATE OR REPLACE FUNCTION afterDeleteArrangements()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+AS $$
+BEGIN
+-- Delete any secret identity of the agent 
+    DELETE FROM People P 
+    WHERE P.PersonID = OLD.secretIdentity;
+
+    -- Adding the agent back to the database with status ghost and reversed codename after it is deleted
+    INSERT INTO Agents(AgentID, codename, designation, killLicense, status, secretIdentity, GenderID)
+    VALUES(default, recurReverseCodename(OLD.codename), OLD.designation, OLD.killLicense, 'ghost', NULL, OLD.GenderID);
+
+    RETURN OLD;
+END;
+$$;
 
 
-DROP TRIGGER IF EXISTS defectiveAgentTrigger ON Agents;
+--DROP TRIGGER IF EXISTS defectiveAgentTrigger ON Agents;
 CREATE TRIGGER defectiveAgentTrigger
     BEFORE DELETE ON Agents
     FOR EACH ROW
     EXECUTE FUNCTION defectiveAgentArrangements();
 
 
-DROP TRIGGER IF EXISTS afterDefectiveAgentTrigger ON Agents;
+--DROP TRIGGER IF EXISTS afterDefectiveAgentTrigger ON Agents;
 CREATE TRIGGER afterDefectiveAgentTrigger
     AFTER DELETE ON Agents
     FOR EACH ROW
     EXECUTE FUNCTION afterDeleteArrangements();
 
 
-BEGIN;
+select
+    9 as Query;
 
-SELECT * FROM Agents A WHERE A.AgentID = 1;
-
-SELECT CaseID FROM Cases where AgentID =1;
-SELECT CaseID FROM Cases where AgentID =73;
-
-DELETE FROM Agents A WHERE A.AgentID = 1;
-SELECT * FROM Agents A WHERE A.secretIdentity = 4941;
-SELECT * FROM Cases C where C.AgentID = 1;
-SELECT * FROM InvolvedIn WHERe PersonID = 4941;
-
-SELECT * FROM Agents WHERE AgentID = 73;
-
-SELECT C1.AgentID, COUNT(C1.AgentID)
-                    FROM Cases C1
-                    GROUP BY C1.AgentID
-                    ORDER BY COUNT(C1.AgentID) ASC;
-
-SELECT CaseID FROM Cases where AgentID =1;
-SELECT * FROM Agents where status = 'ghost';
-
-
-
-DELETE FROM Agents A WHERE A.status = 'ghost';
-SELECT * FROM Agents where status = 'ghost';
-
-ROLLBACK;
-
-
-
-
--- 9.
 CREATE OR REPLACE FUNCTION yearsSinceCaseInTown(town VARCHAR(255))
 RETURNS INT
 LANGUAGE plpgsql
@@ -392,31 +299,41 @@ BEGIN
 END;
 $$;
 
-BEGIN;
-SELECT * FROM yearsSinceCaseInTown('Fosshóll');
-SELECT * FROM Cases where locationID = 47;
-ROLLBACK;
+select
+    10 as Query;
 
+CREATE OR REPLACE FUNCTION FrenemiesOfFrenemiesHelper(PersonID_in_helper INT)
+RETURNS TABLE(personID INT, name VARCHAR(255), proffID INT, genderID INT, LocID INT)
+LANGUAGE plpgsql
+AS $$
+DECLARE CURRENT_PERSONID INT;
+BEGIN
+    for CURRENT_PERSONID in (SELECT distinct P.PersonID
+        FROM InvolvedIn I
+        INNER JOIN People P ON P.PersonID = I.PersonID
+        WHERE I.CaseID IN (SELECT I2.CaseID
+                            FROM InvolvedIn I2
+                            WHERE I2.PersonID = personID_in_helper))
+        loop
+        RETURN QUERY (SELECT distinct P1.*
+                    FROM People P1
+                    INNER JOIN InvolvedIn I3 ON P1.PersonID = I3.PersonID
+                    WHERE P1.PersonID <> PersonID_in_helper AND I3.CaseID IN (SELECT I4.CaseID
+                                        FROM InvolvedIn I4
+                                        WHERE I4.PersonID = CURRENT_PERSONID));
+        END LOOP;
+END;
+$$;
 
-
--- 10.
 
 CREATE OR REPLACE FUNCTION FrenemiesOfFrenemies(PersonID_in INT)
 RETURNS TABLE(personID INT, name VARCHAR(255), proffID INT, genderID INT, LocID INT)
 LANGUAGE plpgsql
 AS $$
 BEGIN
-    FOR r in (SELECT distinct P.PersonID
-    FROM InvolvedIn I
-    INNER JOIN People P ON P.PersonID = I.PersonID
-    WHERE I.CaseID IN (SELECT I2.CaseID
-                        FROM InvolvedIn I2
-                        WHERE I2.PersonID = personID_in))
-    FOR r in return_table loop
-
-    
-
-
+    RETURN QUERY (SELECT distinct * FROM FrenemiesOfFrenemiesHelper(PersonID_in));
 
 END;
 $$;
+
+SELECT * FROM FrenemiesOfFrenemies(4256);
